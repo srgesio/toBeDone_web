bd2fe38ed665a5f111d8073b7f6a54c79640447d
20171014040038 <gesio.m@academico.ifrn.edu.br>
2020-10-12 15:19:48 -0300

    projeto React criado

---
+++ tobedone_web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>To Be Done!</h1>
+      <h3>Sempre existirão coisas interessantes para se fazer!</h3>
+    </div>
+  );
+}
+
+export default App;
